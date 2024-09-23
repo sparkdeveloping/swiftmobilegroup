01016fd259742239c5e31c61d413a67b48bd6f39
Denzel <denzelnyatsanza@gmail.com>
2024-09-23 11:28:42 -0500

    first commit

---
+++ data/index.ts
@@ -0,0 +1,168 @@
+export const navItems = [
+  { name: "Swift", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b11.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Built with global networking technologies",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Smartphone",
+    description: "OUR LATEST",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "For that farmer, in the toughness of any environment, yet connected!",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Our Latest line up",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b55.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "24/7 Tech Support",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Black Bear",
+    des: "Enterprise Grade",
+    img: "/grey.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://github.com/adrianhajdin?tab=repositories",
+  },
+  {
+    id: 2,
+    title: "Silver Fox",
+    des: "Available on Consignment",
+    img: "/silver.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://github.com/adrianhajdin/zoom-clone",
+  },
+  {
+    id: 3,
+    title: "Grey Wolf",
+    des: "Available on Consignment",
+    img: "/black.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://github.com/adrianhajdin/ai_saas_app",
+  },
+
+];
+
+export const testimonials = [
+  {
+      "quote": "Our collaboration with Swift Mobile is transforming how farmers interact with markets and improve their productivity in the global digital economy.",
+      "name": "Richard L. Lackey",
+      "title": "CEO, World Food Bank"
+    },
+    {
+      quote: "All Swift phones come integrated with ZakaPay, offering users the ability to finance their devices through affordable, manageable payments, ensuring accessibility to premium technology even in underserved communities.",
+      name: "ZakaPay"
+    },
+    {
+      quote: "Swift Mobile collaborates with Hamara Group to provide agricultural solutions that empower farmers through innovative technology, bridging the gap between rural areas and global markets.",
+      name: "Hamara Group"
+    },
+    {
+      quote: "Through our partnership with Farm Africa, we support farmers in developing regions by providing devices that facilitate access to valuable data and real-time market information, enhancing agricultural productivity and sustainability.",
+      name: "Farm Africa"
+    }
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "World Food Bank",
+    img: "/worldfoodbank.png",
+    nameImg: "/worldfoodbank.png",
+  },
+  {
+    id: 2,
+    name: "Farm Africa",
+    img: "/farmafrica.png",
+    nameImg: "/farmafrica.png",
+  }
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Police",
+    desc: "This is dummy text This is dummy text This is dummy text This is dummy text This is dummy text This is dummy text",
+    className: "md:col-span-2",
+    thumbnail: "/police.png",
+  },
+  {
+    id: 2,
+    title: "Military",
+    desc: "This is dummy text This is dummy text This is dummy text This is dummy text This is dummy text This is dummy text",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/military.png",
+  },
+  {
+    id: 3,
+    title: "Construction",
+    desc: "This is dummy text This is dummy text This is dummy text This is dummy text This is dummy text This is dummy text",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/construction.png",
+  },
+  {
+    id: 4,
+    title: "Agriculture",
+    desc: "This is dummy text This is dummy text This is dummy text This is dummy text This is dummy text This is dummy text",
+    className: "md:col-span-2",
+    thumbnail: "/agriculture.png",
+  },
+];
+
+export const socialMedia = [
+  
+];
